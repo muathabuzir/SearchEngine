2203d6144adf3b8e1ec2b6d86d57710a48840434
muathabuzir <muath.abuzir@mawdoo3.com>
2019-07-09 17:23:52 +0300

    Update Composer File

--- src/Controllers/SearchResultController.php
+++ src/Controllers/SearchResultController.php
@@ -23,17 +23,16 @@ class SearchResultController extends Controller {
             'search' => 'required|max:250'
         ]);
 
-        #$data = $google->getData($request->search);
+        $data = $google->getData($request->search);
 
-        $data = '{"status":1,"message":"sucess","data":{"items":[{"title":"AMMNA \u2013 Australian Mens & Mixed Netball Association","link":"http:\/\/www.ammna.com.au\/","snippet":"AMMNA News. \ufeffAMMNA Awards draws curtain on 2019 Australian \nChampionships April 30, 2019; Australian Championships Live Stream April 18, \n2019; Game\u00a0..."},{"title":"2015 AMMNA Netball Championships Mens Highlights - YouTube","link":"https:\/\/www.youtube.com\/watch?v=nxlRH08m81s","snippet":"May 27, 2015 ... 2015 AMMNA Netball Championships Mens Highlights. MatthewBayard. Loading\n... Unsubscribe from MatthewBayard? Cancel Unsubscribe."}]}}';
-        $data = json_decode($data);
+        #$data = '{"status":1,"message":"sucess","data":{"items":[{"title":"AMMNA \u2013 Australian Mens & Mixed Netball Association","link":"http:\/\/www.ammna.com.au\/","snippet":"AMMNA News. \ufeffAMMNA Awards draws curtain on 2019 Australian \nChampionships April 30, 2019; Australian Championships Live Stream April 18, \n2019; Game\u00a0..."},{"title":"2015 AMMNA Netball Championships Mens Highlights - YouTube","link":"https:\/\/www.youtube.com\/watch?v=nxlRH08m81s","snippet":"May 27, 2015 ... 2015 AMMNA Netball Championships Mens Highlights. MatthewBayard. Loading\n... Unsubscribe from MatthewBayard? Cancel Unsubscribe."}]}}';
+        #$data = json_decode($data);
 
-        $search =  array_slice($data->data->items, 0, 10);
+        #$search =  array_slice($data->data->items, 0, 10);
         
-        /*$search = array();
+        $search = array();
         if ($data['status'] == 1 && !empty($data['data']->items))
             $search = array_slice($data['data']->items, 0, 10);
-        */
 
         return view('sp_mawdoo3_laravel::search.index', ['data' => $search]);
     }
